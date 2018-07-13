3edb36e46352ea4bf754844034a9ad66318af6ab
XUHO3 <howells.xu@oocl.com>
2018-07-13 19:59:40 +0800

    build ParkingLot framework

---
+++ src/main/java/views/Main.java
@@ -0,0 +1,16 @@
+package views;
+
+import com.google.inject.Injector;
+import controllers.ParkingCarController;
+
+import static com.google.inject.Guice.createInjector;
+
+public class Main {
+    public static void main(String[] args) {
+        Injector injector=createInjector(new ParkingLotModule());
+        ParkingCarController  controller=injector.getInstance(ParkingCarController.class);
+        controller.beginUi();
+
+
+    }
+}
---
+++ src/main/java/views/ParkingLotModule.java
@@ -0,0 +1,9 @@
+package views;
+
+import com.google.inject.AbstractModule;
+
+public class ParkingLotModule extends AbstractModule {
+    @Override
+    protected void configure() {
+    }
+}
