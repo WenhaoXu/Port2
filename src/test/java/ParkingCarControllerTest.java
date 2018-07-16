cf73da793f27084e74e7be717756e1a48ff12d1d
XUHO3 <howells.xu@oocl.com>
2018-07-16 18:36:00 +0800

    add test funcation

--- src/test/java/ParkingCarControllerTest.java
+++ src/test/java/ParkingCarControllerTest.java
@@ -1,16 +1,21 @@
-import com.google.inject.Injector;
 import controllers.ParkingCarController;
 import org.junit.jupiter.api.Test;
-import views.ParkingLotModule;
-
-import static com.google.inject.Guice.createInjector;
+import org.mockito.Mockito;
+import services.ParkingCarService;
+import services.ParkingLotService;
+import views.ParkingLotView;
 
 public class ParkingCarControllerTest {
     @Test
-    public void should_return_beginUI_when_start(){
-        Injector injector=createInjector(new ParkingLotModule());
-        ParkingCarController controller=injector.getInstance(ParkingCarController.class);
-        controller.beginUi();
+    public void should_return_manageView_when_ParkCarService_is_start(){
+        ParkingCarService parkingCarService=Mockito.mock(ParkingCarService.class) ;
+        ParkingLotService parkingLotService=Mockito.mock(ParkingLotService.class);
+        ParkingLotView parkingLotView=Mockito.mock(ParkingLotView.class);
+        Mockito.when(parkingCarService.ParkingCar("1321421")).thenReturn("停车成功，您的小票是：\n");
+        ParkingCarController parkingCarController=new ParkingCarController(parkingLotView,parkingCarService,parkingLotService);
+        parkingCarController.ParkCarService("1321421");
+        Mockito.verify(parkingCarService).ParkingCar("1321421");
+        
     }
 
 
--- src/test/java/RouteTest.java
+++ src/test/java/RouteTest.java
@@ -7,7 +7,7 @@ import views.Route;
 public class RouteTest {
 
     @Test
-    public void should_return_parkingPage_when_input_1(){
+    public void should_return_beginUi_when_start(){
         String currentPage="main";
         ParkingCarController controller=Mockito.mock(ParkingCarController.class);
         Mockito.when(controller.getParkingService()).thenReturn("ParkingServiceView");
@@ -15,6 +15,36 @@ public class RouteTest {
         Route route=new Route(controller,currentPage);
         Request request= new Request("1");
         Mockito.verify(controller).beginUi();
-//        Mockito.verify(controller).getParkCarService();
+
     }
+
+    @Test
+    public void should_return_ParkingView_when_input_1(){
+        String currentPage="main";
+        ParkingCarController controller=Mockito.mock(ParkingCarController.class);
+        Mockito.when(controller.getParkingService()).thenReturn("ParkingServiceView");
+        Mockito.when(controller.beginUi()).thenReturn("manageView");
+        Route route=new Route(controller,currentPage);
+        Request request= new Request("1");
+        route.start(request);
+        Mockito.verify(controller).beginUi();
+        Mockito.verify(controller).getParkingService();
+
+    }
+
+    @Test
+    public void should_return_manageView_when_input_2(){
+        String currentPage="main";
+        ParkingCarController controller=Mockito.mock(ParkingCarController.class);
+        Mockito.when(controller.getManageService()).thenReturn("ManageLotServiceView");
+        Mockito.when(controller.beginUi()).thenReturn("manageView");
+        Route route=new Route(controller,currentPage);
+        Request request= new Request("2");
+        route.start(request);
+        Mockito.verify(controller).beginUi();
+        Mockito.verify(controller).getManageService();
+
+    }
+
+
 }
